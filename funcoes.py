959388d30516e3cdc8c330617352c42d037ac1ef
MariaClaraHiranoMotta <mariachm@al.insper.edu.br>
2024-01-22 16:31:59 -0300

    funcao preenche frota e faz jogada

--- funcoes.py
+++ funcoes.py
@@ -11,7 +11,7 @@ def define_posicoes(dic_info):
 
     if orientacao == "vertical" :
         for i in range(linha, linha+tamanho):
-        #nosso i vai ser a linha que vai aumentar , pq a coluna nao vai mudar de posicao
+        # nosso i vai ser a linha que vai aumentar , pq a coluna nao vai mudar de posicao
             lista_posicao.append([i,coluna])
 
     if orientacao == "horizontal":
@@ -20,3 +20,26 @@ def define_posicoes(dic_info):
             lista_posicao.append([linha,a ])
     
     return lista_posicao
+
+#preencha frota
+def preenche_frota(dic_info, nome , lista_frota):
+    new_dic = {}
+
+    posicao = define_posicoes(dic_info)
+
+    new_dic["tipo"] = nome
+    new_dic["posicoes"] = posicao
+
+    lista_frota.append(new_dic)
+
+    return lista_frota
+
+#faz jogada 
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = "-"
+
+    elif tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = "X"
+
+    return tabuleiro
