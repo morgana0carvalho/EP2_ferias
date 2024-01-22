d6b88cda5a10424d035c65eebf00f4fb49693c67
MariaClaraHiranoMotta <mariachm@al.insper.edu.br>
2024-01-22 14:54:57 -0300

    funcao(define posicoes)

---
+++ funcoes.py
@@ -0,0 +1,22 @@
+#defina posiçao 
+def define_posicoes(dic_info):
+    lista_posicao = []
+
+
+    #acessando o valor 
+    linha = dic_info["linha"]
+    coluna = dic_info["coluna"]
+    orientacao = dic_info["orientacao"]
+    tamanho = dic_info["tamanho"]
+
+    if orientacao == "vertical" :
+        for i in range(linha, linha+tamanho):
+        #nosso i vai ser a linha que vai aumentar , pq a coluna nao vai mudar de posicao
+            lista_posicao.append([i,coluna])
+
+    if orientacao == "horizontal":
+        for a in range(coluna , coluna+ tamanho):
+            #agr nosso a que vai aumentando o numero da coluna conforme o tamanho 
+            lista_posicao.append([linha,a ])
+    
+    return lista_posicao
