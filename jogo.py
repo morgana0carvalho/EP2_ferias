6b48fac915f2fa5a21cd3e469b4ad8de20643a09
MariaClaraHiranoMotta <mariachm@al.insper.edu.br>
2024-01-28 22:11:40 -0300

    jogo funcional

--- jogo.py
+++ jogo.py
@@ -8,10 +8,12 @@ frota_oponente = gerando_frota_automaticamente()
 # Criando tabuleiro com as frotas posicionadas
 tabuleiro_jogador = posiciona_frota(frota_jogador)
 tabuleiro_oponente = posiciona_frota(frota_oponente)
-
 jogando = True
 nao_inedita = True
-lista_posicao_resposta = [] 
+nao_venceu = True
+ja_existe = True
+lista_posicao_resposta = []
+lista_sorteadas = []
 
 while jogando:
     #confirindo se as coordenadas sao ineditas ou nao 
@@ -20,8 +22,9 @@ while jogando:
     print(display)
 
     nao_inedita = True
+    ja_existe = True
     while nao_inedita: 
-    
+        # VEZ DO JOGADOR
         #perguntando a linha
         pergunta_linha = int(input("Em qual linha deseja atacar: ?"))
 
@@ -39,18 +42,40 @@ while jogando:
 
         coordenadas =[pergunta_linha,pergunta_coluna]
         
-        if coordenadas not in lista_posicao_resposta:
-            lista_posicao_resposta.append(coordenadas)
-            nao_inedita = False
-        else:
+
+        if coordenadas in lista_posicao_resposta:
             print(f"A posição linha {pergunta_linha} e coluna {pergunta_coluna} já foi informada anteriormente!")
+        else:
+            lista_posicao_resposta.append(coordenadas)
 
-    tabuleiro_oponente =  faz_jogada(tabuleiro_oponente, pergunta_linha, pergunta_coluna)
+            tabuleiro_oponente =  faz_jogada(tabuleiro_oponente, pergunta_linha, pergunta_coluna)
+            afundou = afundados(frota_oponente, tabuleiro_oponente)
+    
+            if afundou == len(frota_oponente):
+                print('Parabéns! Você derrubou todos os navios do seu oponente!')
+                jogando = False
+                nao_venceu = False
+            nao_inedita = False
 
+    if nao_venceu == True:
+        while ja_existe == True:
+            #VEZ DO OPONENTE
+            linha = random.randint(0, 9)
+            coluna = random.randint(0, 9)
 
-    afundou = afundados(frota_oponente, tabuleiro_oponente)
+            coordenadas = [linha , coluna]
+            
+            if coordenadas not in lista_sorteadas:
+                lista_sorteadas.append(coordenadas)
 
-    if afundou == len(frota_oponente):
-        print('Parabéns! Você derrubou todos os navios do seu oponente!')
-        jogando = False
-                
+                tabuleiro_jogador = faz_jogada(tabuleiro_jogador, linha, coluna)
+                print(f'Seu oponente está atacando na linha {linha} e coluna {coluna}')
+                    
+                afundou_no_mapa_do_jogador = afundados(frota_jogador, tabuleiro_jogador)
+                if afundou_no_mapa_do_jogador == len(frota_jogador):
+                    print('Xi! O oponente derrubou toda a sua frota =(')
+                    jogando = False
+                nao_inedita = False 
+                ja_existe = False
+        
+    
